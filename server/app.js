3798da55c1fcaeb6c77db647b3923d52fa547f56
Vinicyus <vinicyusads11@gmail.com>
2024-04-29 15:49:02 -0300

    criando models

--- server/app.js
+++ server/app.js
@@ -10,10 +10,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Rotas
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
-// ... outras rotas
+const bagItemRoutes = require('./routes/bagItemRoutes');
+const bagRoutes = require('./routes/bagRoutes');
+const branchRoutes = require('./routes/branchRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const orderItemRoutes = require('./routes/categoryRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const stockRoutes = require('./routes/stockRoutes');
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/bagItem', bagItemRoutes)
+app.use('/api/bag', bagRoutes);
+app.use('/api/branch', branchRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/orderItem', orderItemRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/stock', stockRoutes);
+
 // ... configure outras rotas
 
 module.exports = app;
---
+++ server/controllers/userController.js
@@ -0,0 +1,67 @@
+const User = require('../models/User'); // Importando o modelo de Usuário
+
+// Adicionar um novo usuário
+exports.createUser = async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(201).send(newUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+// Obter a lista de usuários
+exports.getUsers = async (_req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+// Obter um único usuário pelo ID
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+// Atualizar um usuário pelo ID
+exports.updateUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+// Deletar um usuário pelo ID
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.status(200).send({ message: 'Usuário deletado com sucesso' });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+// ...outros métodos específicos para a entidade Usuário
+
