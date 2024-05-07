e1110ab379b4203e0ce910f3aebb6281c0035dfe
Vinicyus <vinicyusads11@gmail.com>
2024-05-07 13:12:13 -0300

    server.js e conexao

--- server/connection/db.js
+++ server/connection/db.js
@@ -1,18 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect('sua-uri-de-conexão', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-      // outras opções de configuração...
-    });
-    console.log('MongoDB conectado com sucesso.');
-  } catch (err) {
-    console.error('Conexão com MongoDB falhou:', err.message);
-    // Encerra o processo com falha
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+const uri = 'uri-mini';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conexão com MongoDB estabelecida com sucesso'))
+  .catch(err => console.error('Erro ao conectar com MongoDB', err));
