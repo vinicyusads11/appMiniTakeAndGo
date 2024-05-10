52dacb2e2b37b1903f90c75e9aaa6e9d0f8707bd
Guilherme <guilhermedclarroide@gmail.com>
2024-05-10 17:43:05 -0300

    tela de carregamento e icone do app

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <View>
+        <Text>My App</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
