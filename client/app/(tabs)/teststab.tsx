17b113b152e1a022f479b987f837b243f8846bc1
Guilherme <guilhermedclarroide@gmail.com>
2024-09-23 18:07:22 -0300

    arquivo para testar telas

--- client/app/(tabs)/teststab.tsx
+++ client/app/(tabs)/teststab.tsx
@@ -1,17 +1,19 @@
 //* ARQUIVO PARA TESTAR AS TELAS
 import * as React from "react";
-//import Step1 from "../../screens/Step1";// Caminho relativo 
-//import Step2 from "../../screens/Step2";// Caminho relativo 
-//import Step3 from "../../screens/Step3";// Caminho relativo 
-//import LoginScreen from "../../screens/LoginScreen";
-import RegisterScreen from "../../screens/RegisterScreen";
 //import Welcome from "../../screens/Welcome"; // Caminho relativo 
+//import Step1 from "../../screens/Step1";
+//import Step2 from "../../screens/Step2";
+//import Step3 from "../../screens/Step3";
+//import RegisterScreen from "../../screens/RegisterScreen";
+//import LoginScreen from "../../screens/LoginScreen";
+import BranchStep from "../../screens/BranchStep";
 
 export default function Index() {
+  //return <Welcome />;
   //return <Step1 />;
   //return <Step2 />;
   //return <Step3 />;
+  //return <RegisterScreen/>;
   //return <LoginScreen />;
-  return <RegisterScreen/>;
-  //return <Welcome />;
+  return <BranchStep />;
 } // Renderiza o componente 
