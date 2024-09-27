7a6a1aed2125f5056c60a7971fd5b2e1e2118a8c
Guilherme <guilhermedclarroide@gmail.com>
2024-09-27 16:21:33 -0300

    removendo import inutilizado

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
 import { FontAwesome, SimpleLineIcons, AntDesign, MaterialIcons } from '@expo/vector-icons';
 
 export default function Layout() {
