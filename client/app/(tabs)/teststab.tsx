59758e779206bfa72eb4ba2e467366f71917fb6b
Guilherme <guilhermedclarroide@gmail.com>
2024-10-10 18:18:05 -0300

    arquivo para testar as telas

--- client/app/(tabs)/teststab.tsx
+++ client/app/(tabs)/teststab.tsx
@@ -1,10 +1,10 @@
 //* ARQUIVO PARA TESTAR AS TELAS
-import * as React from "react";
-//import Welcome from "../../screens/Welcome"; // Caminho relativo 
+import * as React from 'react';
+import Welcome from '../Welcome'; // Caminho relativo
 //import Step1 from "../../screens/Step1";
 //import Step2 from "../../screens/Step2";
 //import Step3 from "../../screens/Step3";
-import Register from "../../screens/Register";
+//import Register from "../../screens/Register";
 //import Login from "../../screens/Login";
 //import BranchStep from "../../screens/BranchStep";
 //import ScanBranch1 from "../../screens/ScanBranch1";
@@ -14,14 +14,12 @@ import Register from "../../screens/Register";
 //import OrderCompleted from "../../screens/OrderCompleted";
 //import OrderInfo from "../../screens/OrderInfo";
 
-
-
 export default function Index() {
-  //return <Welcome />;
+  return <Welcome />;
   //return <Step1 />;
   //return <Step2 />;
   //return <Step3 />;
-  return <Register />;
+  //return <Register />;
   //return <Login />;
   //return <BranchStep />;
   //return <ScanBranch1 />;
@@ -30,6 +28,5 @@ export default function Index() {
   //return <Payment />;
   //return <OrderCompleted />;
   //return <OrderInfo />;
-} 
-
-// Renderiza o componente 
+}
+// Renderiza o componente
