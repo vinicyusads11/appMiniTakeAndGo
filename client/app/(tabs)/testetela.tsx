5db391df9c05abd71b118075223109780567e44c
Guilherme <guilhermedclarroide@gmail.com>
2024-10-21 17:28:00 -0300

    arquivo para testar telas

---
+++ client/app/(tabs)/testetela.tsx
@@ -0,0 +1,24 @@
+//* ARQUIVO PARA TESTAR AS TELAS
+import * as React from 'react';
+import Welcome from '../screens/Welcome'; // Caminho relativo
+//import Step1 from "../screens/Step1";
+//import Step2 from "../screens/Step2";
+//import Step3 from "../screens/Step3";
+//import ScanBarCode from "../screens/ScanBarCode";
+//import EmptyBasket from "../screens/EmptyBasket";
+//import Payment from "../screens/Payment";
+//import OrderCompleted from "../screens/OrderCompleted";
+//import OrderInfo from "../screens/OrderInfo";
+
+export default function Index() {
+  return <Welcome />;
+  //return <Step1 />;
+  //return <Step2 />;
+  //return <Step3 />;
+  //return <ScanBarCode />;
+  //return <EmptyBasket />;
+  //return <Payment />;
+  //return <OrderCompleted />;
+  //return <OrderInfo />;
+}
+// Renderiza o componente
