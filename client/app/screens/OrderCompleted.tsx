ef9359c5d42a37de7b5bf96e04435164eae105b5
Guilherme <guilhermedclarroide@gmail.com>
2024-10-21 17:30:13 -0300

    adicionados e alterados de pasta

---
+++ client/app/screens/OrderCompleted.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { Image } from 'expo-image';
+import styles from '../../styles/OrderCompletedStyle';
+
+const OrderCompleted = () => {
+  return (
+    <View style={styles.ordercompleted}>
+      <View style={styles.ordercompletedChild} />
+      <Text style={[styles.verItensComprados, styles.verItensCompradosFlexBox]}>
+        Ver itens comprados
+      </Text>
+      <Image
+        style={styles.logotipoPretoSemFundo1}
+        contentFit="cover"
+        source={require('../../assets/logopretosemfundo.png')}
+      />
+      <Text style={[styles.pagamentoAprovado, styles.verItensCompradosFlexBox]}>
+        Pagamento aprovado!
+      </Text>
+      <Image
+        style={styles.iconCheckmarkCircle}
+        contentFit="cover"
+        source={require('../../assets/checkmarkicon.png')}
+      />
+    </View>
+  );
+};
+
+export default OrderCompleted;
