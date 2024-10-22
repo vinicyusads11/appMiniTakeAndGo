090035472de32fc591ae2ddf18d63d6378210bde
Guilherme <guilhermedclarroide@gmail.com>
2024-10-22 12:27:30 -0300

    arquivo para testar as telas

--- client/app/(tabs)/testetela.tsx
+++ client/app/(tabs)/testetela.tsx
@@ -1,21 +1,21 @@
 //* ARQUIVO PARA TESTAR AS TELAS
 import * as React from 'react';
-import Welcome from '../screens/Welcome'; // Caminho relativo
+//import Welcome from '../screens/Welcome'; // Caminho relativo
 //import Step1 from "../screens/Step1";
 //import Step2 from "../screens/Step2";
 //import Step3 from "../screens/Step3";
-//import ScanBarCode from "../screens/ScanBarCode";
+import ScanBarCode from "../screens/ScanBarCode";
 //import EmptyBasket from "../screens/EmptyBasket";
 //import Payment from "../screens/Payment";
 //import OrderCompleted from "../screens/OrderCompleted";
 //import OrderInfo from "../screens/OrderInfo";
 
 export default function Index() {
-  return <Welcome />;
+  //return <Welcome />;
   //return <Step1 />;
   //return <Step2 />;
   //return <Step3 />;
-  //return <ScanBarCode />;
+  return <ScanBarCode />;
   //return <EmptyBasket />;
   //return <Payment />;
   //return <OrderCompleted />;
