c4eda59b05464d84a8d7223281b147246e9c46ed
Guilherme <guilhermedclarroide@gmail.com>
2024-10-31 18:25:18 -0300

    configura segunda tela do tutorial com instruções para conferir e finalizar pedido, ajuste de layout e barra de progresso animada

--- client/app/screens/Step2.js
+++ client/app/screens/Step2.js
@@ -1,43 +1,33 @@
 import * as React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { Image } from 'expo-image';
-import { Text, Pressable, View } from 'react-native';
 import styles from '../../styles/Step2Style';
 import { useRouter } from 'expo-router';
+import ProgressBar from '../../components/ProgressBar';
 
 const Step2 = () => {
   const router = useRouter();
   return (
     <View style={styles.step2}>
+      <ProgressBar step={2} />
+
+      <Text style={styles.confiraTodosOs}>
+        Confira todos os produtos, confirme e finalize sua compra
+      </Text>
+
+      <TouchableOpacity style={styles.voltar} onPress={() => router.push('../screens/Step1')}>
+        <Text style={styles.voltarTexto}>VOLTAR</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.prximo} onPress={() => router.push('../screens/Step3')}>
+        <Text style={styles.prximoTexto}>PRÓXIMO</Text>
+      </TouchableOpacity>
+
       <Image
         style={styles.step2Child}
         contentFit="cover"
         source={require('../../assets/listacomprascestaicon.png')}
       />
-      <Pressable style={styles.voltar} onPress={() => router.push('../screens/Step1')}>
-        <Text style={[styles.voltar1, styles.voltar1Typo]}>VOLTAR</Text>
-      </Pressable>
-      <Image
-        style={styles.step2Item}
-        contentFit="cover"
-        source={require('../../assets/ellipse-10.png')}
-      />
-      <Image
-        style={[styles.step2Inner, styles.step2InnerLayout]}
-        contentFit="cover"
-        source={require('../../assets/ellipse-11.png')}
-      />
-      <Image
-        style={[styles.ellipseIcon, styles.step2InnerLayout]}
-        contentFit="cover"
-        source={require('../../assets/ellipse-12.png')}
-      />
-      <Text style={styles.text}>2</Text>
-      <View style={[styles.lineView, styles.lineViewLayout]} />
-      <View style={[styles.step2Child1, styles.lineViewLayout]} />
-      <Pressable style={styles.prximo} onPress={() => router.push('../screens/Step3')}>
-        <Text style={[styles.prximo1, styles.voltar1Typo]}>PRÓXIMO</Text>
-      </Pressable>
-      <Text style={styles.confiraTodosOs}>Confira todos os produtos e finalize seu pedido</Text>
     </View>
   );
 };
