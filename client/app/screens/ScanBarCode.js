36efed87c457f532484b5243a3ce6f738d3a33ac
Guilherme <guilhermedclarroide@gmail.com>
2024-10-31 18:24:25 -0300

    implementa funcionalidade de escaneamento de código de barras, tratamento de erros com mensagens toast, redirecionamento ao carrinho e reescaneamento automático

--- client/app/screens/ScanBarCode.js
+++ client/app/screens/ScanBarCode.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, Alert, Pressable } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { CameraView, Camera } from 'expo-camera';
 import { Image } from 'expo-image';
 import { useRouter, useLocalSearchParams } from 'expo-router';
@@ -8,11 +8,12 @@ import styles from '../../styles/ScanBarCodeStyles';
 export default function ScanBarCode() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [cart, setCart] = useState([]); // Estado do carrinho
+  const [cart, setCart] = useState([]);
+  const [toastMessage, setToastMessage] = useState(null);
+  const [scanError, setScanError] = useState(false);
   const router = useRouter();
   const { cart: existingCart } = useLocalSearchParams();
 
-  // Carrega o carrinho existente ao retornar para a tela de escaneamento
   useEffect(() => {
     if (existingCart) {
       setCart(JSON.parse(existingCart));
@@ -28,6 +29,7 @@ export default function ScanBarCode() {
 
   const handleBarcodeScanned = async ({ data }) => {
     setScanned(true);
+    setScanError(false);
     console.log('Código de barras escaneado:', data);
 
     try {
@@ -40,51 +42,51 @@ export default function ScanBarCode() {
       if (response.ok) {
         const product = await response.json();
         if (product) {
-          Alert.alert(
-            'Produto adicionado!',
-            `${product.name}, com o valor de R$ ${product.price.toFixed(
-              2
-            )}, foi adicionado ao carrinho!`
-          );
-
-          // Adiciona o produto ao carrinho e redireciona para a tela do carrinho
-          const updatedCart = [...cart, { ...product, quantity: 1 }]; // Adiciona o produto com quantidade inicial de 1
+          const updatedCart = [...cart, { ...product, quantity: 1 }];
           setCart(updatedCart);
 
-          // Redireciona para a tela de carrinho após um pequeno atraso para exibir o Alert
           setTimeout(() => {
             router.push({
-              pathname: '/screens/Cart',
-              params: { cart: JSON.stringify(updatedCart) }
+              pathname: '../(tabs)/basket',
+              params: { cart: JSON.stringify(updatedCart) },
             });
-          }, 500); // Atraso para exibir o alert antes do redirecionamento
+          }, 500);
         } else {
-          Alert.alert('Erro', 'Produto não encontrado no banco de dados.');
+          // Exibe a mensagem de erro por 5 segundos
+          setToastMessage('Erro: Produto não encontrado no banco de dados.');
+          setScanError(true);
+          setTimeout(() => setToastMessage(null), 5000);
         }
       } else {
-        Alert.alert('Erro', 'Produto não encontrado.');
+        // Exibe a mensagem de erro por 5 segundos
+        setToastMessage('Erro: Produto não encontrado.');
+        setScanError(true);
+        setTimeout(() => setToastMessage(null), 5000);
       }
     } catch (error) {
-      Alert.alert('Erro', 'Falha ao buscar produto.');
+      // Exibe a mensagem de erro por 5 segundos
+      setToastMessage('Erro: Falha ao buscar produto.');
+      setScanError(true);
+      setTimeout(() => setToastMessage(null), 5000);
     }
   };
 
   if (hasPermission === null) {
-    return <Text>Solicitando acesso a camera...</Text>;
+    return <Text>Solicitando acesso à câmera...</Text>;
   }
   if (hasPermission === false) {
-    return <Text>Sem acesso a câmera</Text>;
+    return <Text>Sem acesso à câmera</Text>;
   }
 
   return (
     <View style={styles.scanbarcode}>
-      <Pressable onPress={() => router.push('/(tabs)/home')}>
+      <TouchableOpacity onPress={() => router.push('/(tabs)/home')}>
         <Image
           style={styles.scanbarcodeChild}
           contentFit="cover"
           source={require('../../assets/vector-36.png')}
         />
-      </Pressable>
+      </TouchableOpacity>
       <Text style={[styles.aponteParaOContainer, styles.button1Typo]}>
         <Text style={styles.aponteParaO}>
           <Text style={styles.aponteParaO1}>Aponte para o</Text>
@@ -101,31 +103,39 @@ export default function ScanBarCode() {
         onBarcodeScanned={scanned ? undefined : handleBarcodeScanned}
         barcodeScannerSettings={{
           barcodeTypes: [
-            'qr', // QR Code (Quick Response Code)
-            'ean13', // EAN-13 (European Article Number 13) - Comum em produtos de mercado
-            'ean8', // EAN-8 (European Article Number 8) - Versão reduzida do EAN-13
-            'upc_a', // UPC-A (Universal Product Code A) - Comum nos EUA e Canadá
-            'upc_e', // UPC-E (Universal Product Code E) - Versão compacta do UPC-A
-            'code128', // Code 128 - Usado em transporte, logística e etiquetas de remessa
-            'code39', // Code 39 - Código alfanumérico usado em indústrias e inventário
-            'code93', // Code 93 - Versão compacta e mais eficiente do Code 39
-            'pdf417', // PDF417 - Código bidimensional, usado em documentos como carteiras de motorista
-            'itf14', // ITF-14 (Interleaved 2 of 5) - Usado para identificar embalagens e caixas no comércio
+            'qr',
+            'ean13',
+            'ean8',
+            'upc_a',
+            'upc_e',
+            'code128',
+            'code39',
+            'code93',
+            'pdf417',
+            'itf14',
           ],
         }}
         style={StyleSheet.absoluteFillObject}
       />
-      {scanned && (
-        <Button title={'Clique aqui para escanear novamente'} onPress={() => setScanned(false)} />
-      )}
-      <Pressable
-        onPress={() => router.push('/(tabs)/home')}
-        style={localStyles.backButtonContainer}
-      >
-        <View style={localStyles.backButton}>
-          <Text style={localStyles.backButtonText}>Voltar</Text>
+      {toastMessage && (
+        <View style={localStyles.toast}>
+          <Text style={localStyles.toastText}>{toastMessage}</Text>
         </View>
-      </Pressable>
+      )}
+      <TouchableOpacity onPress={() => router.push('/(tabs)/home')} style={localStyles.backButton}>
+        <Text style={localStyles.backButtonText}>Voltar à Tela Inicial</Text>
+      </TouchableOpacity>
+      {scanError && (
+        <TouchableOpacity
+          style={localStyles.retryButton}
+          onPress={() => {
+            setScanned(false);
+            setScanError(false);
+          }}
+        >
+          <Text style={localStyles.retryButtonText}>CLIQUE AQUI PARA ESCANEAR NOVAMENTE</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -135,51 +145,59 @@ const localStyles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-between',
   },
-  message: {
-    textAlign: 'center',
-    paddingBottom: 10,
-    color: 'black',
-    fontSize: 16,
-  },
-  cameraContainer: {
-    width: '100%',
-    height: 200,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  camera: {
-    width: '100%',
-    height: '100%',
-  },
-  button: {
-    width: '30%',
-    alignItems: 'center',
-    padding: 10,
-    backgroundColor: '#3cb3f6',
-    borderRadius: 5,
-    marginTop: 10,
-  },
-  buttonText: {
-    color: 'white',
-  },
-  backButtonContainer: {
-    position: 'absolute', // Para fixar o botão
-    bottom: 20, // Define a distância da parte inferior
-    left: 0,
-    right: 0,
-    alignItems: 'center',
-  },
   backButton: {
+    position: 'absolute',
+    bottom: 80,
+    left: '50%',
+    transform: [{ translateX: -90 }],
     backgroundColor: '#3cb3f6',
-    padding: 10,
-    borderRadius: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '40%',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 4,
   },
   backButtonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  toast: {
+    position: 'absolute',
+    bottom: 150,
+    left: '10%',
+    right: '10%',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#333',
+    borderRadius: 8,
+    alignItems: 'center',
+    opacity: 0.9,
+  },
+  toastText: {
     color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  retryButton: {
+    position: 'absolute',
+    top: 50,
+    left: '50%',
+    transform: [{ translateX: -150 }],
+    backgroundColor: '#3cb3f6',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+    elevation: 5,
+  },
+  retryButtonText: {
+    color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
