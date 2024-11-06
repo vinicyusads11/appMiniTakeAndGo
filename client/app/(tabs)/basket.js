eca05f1bcb5ecd9e50bebe3b5b8739bab33d1488
Guilherme <guilhermedclarroide@gmail.com>
2024-11-06 20:16:34 -0300

    adição ícone do pix;melhorando clareza das mensagens; UI UX;

--- client/app/(tabs)/basket.js
+++ client/app/(tabs)/basket.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faPix } from '@fortawesome/free-brands-svg-icons';
 
 export default function Basket() {
   const router = useRouter();
@@ -50,7 +52,7 @@ export default function Basket() {
       <View style={styles.emptyCartContainer}>
         <Text style={styles.emptyCartMessage}>A sua cesta de compras está vazia!</Text>
         <Text style={styles.emptyCartInstruction}>
-          Clique no ícone de CÓDIGO DE BARRAS abaixo para escanear seus produtos
+          Clique no ícone de <Text style={styles.codigoDeBarras}>CÓDIGO DE BARRAS</Text> abaixo para escanear seus produtos e adicioná-los à cesta
         </Text>
         <TouchableOpacity
           onPress={() => router.push('/screens/ScanBarCode')}
@@ -64,35 +66,52 @@ export default function Basket() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Carrinho de Compras</Text>
+      <Text style={styles.header}>Cesta</Text>
       <FlatList
         data={cartItems}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <View style={styles.itemContainer}>
+            <Image source={{ uri: item.image_url }} style={styles.itemImage} />
             <Text style={styles.item}>
-              {item.name} - R$ {item.price.toFixed(2)} x {item.quantity}
+              {item.name} - R$ {item.price.toFixed(2)}
             </Text>
             <View style={styles.buttonsContainer}>
               <TouchableOpacity onPress={() => decreaseQuantity(item.id)}>
-                <Text style={styles.buttonText}>-</Text>
+                <Icon name="minus-circle-outline" size={30} color="#3CB3F6" />
               </TouchableOpacity>
+              <Text style={styles.quantityText}>{item.quantity}</Text>
               <TouchableOpacity onPress={() => increaseQuantity(item.id)}>
-                <Text style={styles.buttonText}>+</Text>
+                <Icon name="plus-circle-outline" size={30} color="#3CB3F6" />
               </TouchableOpacity>
             </View>
           </View>
         )}
       />
-      <Text style={styles.total}>Total: R$ {total}</Text>
+
+      {/* Botões de Esvaziar e Escanear */}
       <View style={styles.buttonRow}>
         <TouchableOpacity style={styles.emptyCartButton} onPress={emptyCart}>
-          <Text style={styles.emptyCartButtonText}>Esvaziar Carrinho</Text>
+          <Text style={styles.emptyCartButtonText}>Esvaziar Cesta</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.scanButton} onPress={handleScanAnotherProduct}>
-          <Text style={styles.scanButtonText}>Escanear Outro Produto</Text>
+          <Text style={styles.scanButtonText}>Escanear mais produtos</Text>
         </TouchableOpacity>
       </View>
+
+      {/* Seção de Forma de Pagamento e Valor Total */}
+      <View style={styles.paymentContainer}>
+        <Text style={styles.paymentText}>Forma de Pagamento:</Text>
+        <View style={styles.paymentMethodContainer}>
+          <Text style={styles.paymentMethod}>PIX</Text>
+          <FontAwesomeIcon icon={faPix} size={20} color="#4DB6AC" style={styles.pixIcon} />
+        </View>
+      </View>
+      <View style={styles.totalContainer}>
+        <Text style={styles.totalLabel}>Valor Total da Compra:</Text>
+        <Text style={styles.totalAmount}>R$ {total}</Text>
+      </View>
+
       <TouchableOpacity style={styles.confirmButton} onPress={handleConfirmPurchase}>
         <Text style={styles.confirmButtonText}>Confirmar Compra e Pagar</Text>
       </TouchableOpacity>
@@ -101,18 +120,83 @@ export default function Basket() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 20 },
-  header: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+  },
+  header: { fontSize: 30, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' },
   itemContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginVertical: 5,
+    marginVertical: 10,
   },
-  item: { fontSize: 18 },
-  buttonsContainer: { flexDirection: 'row', alignItems: 'center' },
-  buttonText: { fontSize: 20, padding: 10, color: 'blue', fontWeight: 'bold' },
-  total: { fontSize: 20, fontWeight: 'bold', marginTop: 20 },
+  itemImage: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  item: { fontSize: 18, flex: 1 },
+  buttonsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: 120,
+  },
+  quantityText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#333',
+    marginHorizontal: 10,
+  },
+
+  // Estilos de pagamento e valor total
+  paymentContainer: {
+    marginTop: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    marginBottom: 5,
+  },
+  paymentText: {
+    fontSize: 18,
+    color: '#333',
+  },
+  paymentMethodContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  paymentMethod: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+  pixIcon: {
+    marginLeft: 5,
+  },
+
+  totalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  totalLabel: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  totalAmount: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
   buttonRow: {
     flexDirection: 'row',
     justifyContent: 'space-around',
@@ -139,7 +223,7 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 8,
     alignItems: 'center',
-    marginTop: 30,
+    marginTop: 10,
   },
   confirmButtonText: { color: 'white', fontSize: 18, fontWeight: 'bold' },
 
@@ -149,20 +233,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
+    backgroundColor: '#FFFFFF',
   },
   emptyCartMessage: {
-    fontSize: 24,
+    fontSize: 25,
     fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-  emptyCartInstruction: {
-    fontSize: 16,
-    color: '#555',
     textAlign: 'center',
     marginBottom: 20,
   },
+  emptyCartInstruction: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginLeft:50,
+    marginRight:50,
+  },
+  codigoDeBarras: {
+    color: '#3cb3f6', 
+    fontWeight: 'bold', 
+  },
   barcodeButton: {
-    marginTop: 20,
+    marginTop: 230,
   },
 });
