4adcac8c916e72bced9a381b2772109bf5ca9366
Guilherme <guilhermedclarroide@gmail.com>
2024-11-06 20:20:34 -0300

    melhorando clareza do texto;botão para pular tutorial

--- client/app/screens/Welcome.js
+++ client/app/screens/Welcome.js
@@ -14,10 +14,15 @@ const Welcome = () => {
         source={require('../../assets/logopretosemfundo.png')}
       />
       <Text style={styles.bemVindoAMini}>Bem-vindo à MINI</Text>
-      <Text style={styles.suaNovaForma}>Sua nova forma de fazer compras</Text>
+      <Text style={styles.suaNovaForma}>Sua nova forma de fazer compras!</Text>
       <Text style={styles.sigaAquiCom}>
-        Siga aqui com nosso tutorial para comprar os seus produtos
+        Siga aqui com nosso passo a passo completo para a compra de seus produtos
       </Text>
+
+      <TouchableOpacity style={styles.skipButton} onPress={() => router.push('../(tabs)/home')}>
+        <Text style={styles.skipText}>Pular Tutorial</Text>
+      </TouchableOpacity>
+
       <TouchableOpacity style={styles.prximo} onPress={() => router.push('../screens/Step1')}>
         <Text style={styles.prximoTexto}>PRÓXIMO</Text>
       </TouchableOpacity>
