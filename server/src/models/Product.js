b9c857315e4a0bb103d89b8a184c41455ea91f7a
Guilherme <guilhermedclarroide@gmail.com>
2024-11-13 19:34:53 -0300

    corrigindo importação e exportação

--- server/src/models/Product.js
+++ server/src/models/Product.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
   name: String,
@@ -6,4 +6,4 @@ const productSchema = new mongoose.Schema({
   price: Number
 });
 
-module.exports = mongoose.model('Product', productSchema);
+export default mongoose.model('Product', productSchema);
